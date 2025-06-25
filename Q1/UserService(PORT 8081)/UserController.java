6595ea08501b7d27639e603bc2310a6e625afed8
Shreyash Roy <22052762@kiit.ac.in>
2025-06-25 22:49:10 +0530

    Update UserController.java

--- Q1/UserService(PORT 8081)/UserController.java
+++ Q1/UserService(PORT 8081)/UserController.java
@@ -4,6 +4,6 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User getUser(@PathVariable int id) {
-        return new User(id, "John Doe", "john.doe@example.com");
+        return new User(id, "Shreyash Roy", "shreyashroy1324@gmail.com");
     }
 }
