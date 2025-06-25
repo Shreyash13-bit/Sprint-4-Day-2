4ca48f0b9ccacde38c96e054e0b1d1dfebad86f4
Shreyash Roy <22052762@kiit.ac.in>
2025-06-25 22:43:06 +0530

    Create UserServiceApplication.java

---
+++ Q1/UserService(PORT 8081)/UserServiceApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
