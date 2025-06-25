ad0aeaa39bb133308c49ecf3c89a676bed2c2517
Shreyash Roy <22052762@kiit.ac.in>
2025-06-25 22:45:01 +0530

    Create ProductServiceApplication.java

---
+++ Q1/ProductService(PORT 8082)/ProductServiceApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+}
